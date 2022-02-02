6666d6149a32d7bb632df97b62955a3c0c7bd1be
Nik720 <nikunj.beladiya@tarento.com>
2022-02-02 20:22:59 +0530

    Initial commit with config module

---
+++ src/app.module.ts
@@ -0,0 +1,19 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import configuration from './config/configuration';
+import { UserModule } from './user/user.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [configuration]
+    }),
+    UserModule
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
