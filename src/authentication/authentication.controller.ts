38abf1876acd72d9e6bbd2430c190606a049c2d2
Nik720 <nikunj.beladiya@tarento.com>
2022-02-03 17:37:35 +0530

    Authentication using passport local strategy

--- src/authentication/authentication.controller.ts
+++ src/authentication/authentication.controller.ts
@@ -1,8 +1,9 @@
-import { Body, Controller, HttpCode, Post } from '@nestjs/common';
+import { Body, Controller, HttpCode, Post, Req, UseGuards } from '@nestjs/common';
 import User from 'src/user/entities/user.entity';
 import { AuthenticationService } from './authentication.service';
-import { LoginDto } from './dto/login.dto';
 import { RegisterDto } from './dto/register.dto';
+import { LocalAuthenticationGuard } from './guards/localAuthentication.guard';
+import IRequestWithUser from './interface/requestWithUser.interface';
 
 @Controller('authentication')
 export class AuthenticationController {
@@ -13,14 +14,16 @@ export class AuthenticationController {
 
     @Post('register')
     @HttpCode(200)
-    register(@Body() createUserDto: RegisterDto): Promise<User> {
+    async register(@Body() createUserDto: RegisterDto): Promise<User> {
       return this.authService.register(createUserDto);
     }
 
     @Post('login')
+    @UseGuards(LocalAuthenticationGuard)
     @HttpCode(200)
-    login(@Body() logindto: LoginDto): Promise<User> {
-      return this.authService.getAuthenticatedUser(logindto.email, logindto.password);
+    async login(@Req() request: IRequestWithUser){
+      const user = request.user;
+      return user;
     }
 
 }
--- src/authentication/authentication.module.ts
+++ src/authentication/authentication.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
+import { PassportModule } from '@nestjs/passport';
 import { UserModule } from 'src/user/user.module';
 import { AuthenticationController } from './authentication.controller';
 import { AuthenticationService } from './authentication.service';
+import { LocalStrategy } from './strategy/local.strategy';
 
 @Module({
-  imports: [UserModule],
+  imports: [UserModule, PassportModule],
   controllers: [AuthenticationController],
-  providers: [AuthenticationService]
+  providers: [AuthenticationService, LocalStrategy]
 })
 export class AuthenticationModule {}
