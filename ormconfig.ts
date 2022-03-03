4965c9805da8c3e95cf8088f74104800f8c62699
Nik720 <nikunj.beladiya@tarento.com>
2022-03-03 23:39:47 +0530

    added typeorm config to use migration

---
+++ ormconfig.ts
@@ -0,0 +1,33 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+import { ConnectionOptions } from 'typeorm';
+import * as fs from 'fs';
+
+let config: ConnectionOptions = {
+  type: 'mysql',
+  host: process.env.DATABASE_HOST,
+  port: Number(process.env.DATABASE_PORT),
+  username: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME,
+  synchronize: false, // Auto sync entity changes
+  logging: "all",
+  "entities": ["dist/**/*.entity{ .ts,.js}"],
+  migrations: [__dirname + 'src/database/migrations/**/*{.ts,.js}'],
+  cli: {
+    migrationsDir: 'src/database/migrations',
+    entitiesDir: 'src/entity',
+  },
+};
+
+if (process.env.NODE_ENV === 'production') {
+  config = {
+    ...config,
+    ssl: {
+      ca: fs.readFileSync(process.env.SSL_PATH),
+    },
+  };
+}
+
+export = config;
